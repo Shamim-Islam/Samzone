170f95a62d4fcd868b333c109b5cf5338c1cf5b7
shamim <aminulislamshamim844@gmail.com>
2020-11-28 23:21:03 +0600

    some important changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+/*------------ typing animation script -------------*/
+$(window).on('load', function () {
 
-// typing animation script
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+
+});
+
+/*------------ typing animation script -------------*/
 
 var typed = new Typed(".typing", {
     strings: ["Web Designer.", "Web Developer.", "Freelancer."],
@@ -13,24 +20,34 @@ var typed = new Typed(".typing", {
 /*------------ Sticky-navbar -------------*/
 
 $(document).ready(function () {
-   
+
     $(window).scroll(function () {
-        
-        if(this.scrollY > 20) {
-            
+
+        if (this.scrollY > 150) {
+
             $('.navbar').addClass('sticky-navbar');
             // show back to top btn
             $('#back-to-top').fadeIn();
-            
+
         } else {
-            
+
             $('.navbar').removeClass('sticky-navbar');
             // hide back to top btn
             $('#back-to-top').fadeOut();
         }
     })
-    
+
 });
+
+/*------------ Mobile menu (Pure Javascript) -------------*/
+
+//function toggleMenu() {
+//    var menuToggle = document.querySelector('.toggle');
+//    var navmenu = document.querySelector('.navbar-nav');
+//    menuToggle.classList.toggle('change');
+//    navmenu.classList.toggle('nav-active');
+//} 
+
 
 /*$(document).ready(function () {
    
@@ -50,18 +67,42 @@ $(document).ready(function () {
     
 });*/
 
-/*------------ Mobile menu (Toggler btn) -------------*/
+/*------------ Mobile menu (jQuery) -------------*/
 
 $(document).ready(function () {
 
-    $('.navbar-toggler').click(function () {
+    // show/hide mobile navbar
+    $('.toggle').click(function () {
 
-        $('.navbar-toggler').toggleClass('change');
-        /*$('nav').css('background', 'rgba(33, 34, 38, 0.8)');*/
-        
+        $('.toggle').toggleClass('change');
+        $('.navbar-nav').toggleClass('nav-active');
+
+    });
+
+    // close mobile navbar to click navlink 
+    $('.nav-link').click(function () {
+
+        $('.toggle').toggleClass('change');
+        $('.navbar-nav').toggleClass('nav-active');
+
     });
 
 });
+
+//$(document).ready(function ($) {
+//    
+//    /* toggle nav */
+//    $(".toggle").on("click", function () {
+//        $(".navbar-nav").slideToggle();
+//        $(this).toggleClass("change");
+//    });
+//
+//    $(".navbar-nav").on("click", "li", function () {
+//        $(".navbar-nav").slideToggle();
+//        $(".toggle").click();
+//    });
+//
+//});
 
 /*------------ smooth-scroll -------------*/
 
@@ -124,19 +165,3 @@ $(document).ready(function () {
     });
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
